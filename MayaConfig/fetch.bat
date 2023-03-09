a7444ce93112379ac0296710c0e4279b038b503f
Peter Donald <peter@realityforge.org>
2023-03-09 11:57:11 +1100

    Add scripts to copy Maya preferences localy or the reverse

---
+++ MayaConfig/fetch.bat
@@ -0,0 +1,2 @@
+mkdir prefs\shelves
+copy "%USERPROFILE%\Documents\maya\2023\prefs\shelves\shelf_Custom.mel" prefs\shelves\shelf_Custom.mel
---
+++ MayaConfig/update_maya.bat
@@ -0,0 +1,1 @@
+copy prefs\shelves\shelf_Custom.mel "%USERPROFILE%\Documents\maya\2023\prefs\shelves\shelf_Custom.mel"
